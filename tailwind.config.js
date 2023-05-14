e854c787885b29933d9b8bf0fa571741429140dc
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2023-05-13 21:05:55 -0300

    projeto com tailwindcss completo

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        intro: 'url("../images/Background.png")',
+      },
+      colors: {
+        text_content: "#383838",
+        text_button: "#ADAEAE",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontSize: {
+        "headering-h1": "55px",
+      },
+      lineHeight: {
+        "headering-h1": "83px",
+        "headering-h1-mobile": "54px",
+      },
+      maxWidth: {
+        1140: "1140px",
+        608: "608px",
+        612: "612px",
+        690: "690px",
+      },
+    },
+  },
+  plugins: [],
+};
